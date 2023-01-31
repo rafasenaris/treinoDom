59aeabac382dd36516c649cdbf8838328237701f
Rafael Souza Señaris <rafael.souza.senaris@gmail.com>
2023-01-30 21:32:15 -0300

    ATT 30/01/2022

--- acessandoDom/script.js
+++ acessandoDom/script.js
@@ -1,5 +1,26 @@
 // acessar por tag
 
-let titulo = document.getElementsByTagName("h1");
-
+let titulo = document.getElementsByTagName("h1")[0];
 console.log(titulo);
+
+let lis = document.getElementsByTagName("li");
+console.log(lis[3]);
+
+//id
+let paragrafo = document.getElementById("paragrafo");
+console.log(paragrafo);
+
+//class
+let itensLista = document.getElementsByClassName("item");
+console.log(itensLista);
+
+//querySelectorAll
+let itensQuerry = document.querySelectorAll('#lista2 li');
+console.log(itensQuerry);
+
+let itensQuerry2 = document.querySelectorAll('#lista .item');
+console.log(itensQuerry2);
+
+//querySelector
+let lista = document.querySelector('#lista');
+console.log(lista);
